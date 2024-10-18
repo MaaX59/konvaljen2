e787af49f089522a35a96395f97f588c17be54a7
MaaX59 <Maax.strandberg@gmail.com>
2024-10-18 15:51:25 +0200

    welcome

--- src/pages/front/front.jsx
+++ src/pages/front/front.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Meny from "./meny/Meny";
 import Header from "./header/Header";
+import Welcome from "./welcome/Welcome";
 
 const Front = () => {
   return (
     <div>
       <Meny />
       <Header />
+      <Welcome />
     </div>
   );
 };
---
+++ src/pages/front/welcome/Welcome.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./welcome.css";
+import leftRose from "../../../pics/rose3.png";
+import rightRose from "../../../pics/rose1.png";
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <div className="welcome-leftRose">
+        <img src={leftRose} alt="rose2" />
+      </div>
+      <div className="welcome-text">
+        <h1>Välkomna till Konvaljen</h1>
+        <h3>
+          Sedan 80-talet har vi levererat blommor av högsta kvalitet till kunder
+          över hela Trollhättan. <p />
+          Vi värdesätter personlig service och skapar blomsterarrangemang
+          utifrån era individuella önskemål. Oavsett om du söker blommor till en
+          stor högtid eller lite vardagsglädje, så har vi alltid säsongens
+          vackraste blomster i lager.
+          <p /> Med vårt breda utbud av snittblommor, trendiga krukväxter och
+          dekorativa tillbehör får ni uppleva blomsterglädje på riktigt. <p />
+          Vi brinner för kreativa utmaningar och njuter av alla de underbara
+          möten vi får med er kunder.
+          <p /> Behöver ni beställa blommor och inte kan ta er hit? Inga problem
+          – vi erbjuder snabb hemleverans till hela trestad. Tveka inte att höra
+          av er till oss så hjälper vi er. <p />
+          Välkommen till Konvaljen – Där blommor är vår stora passion i livet!
+        </h3>
+      </div>
+      <div className="welcome-rightRose">
+        <img src={leftRose} alt="rose" />
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
