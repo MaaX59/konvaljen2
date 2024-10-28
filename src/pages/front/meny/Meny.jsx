48b0a2ce57a155858cb3cda7ddeb8e4707f91b35
MaaX59 <Maax.strandberg@gmail.com>
2024-10-28 15:13:53 +0100

    kontakt klar

--- src/pages/front/meny/Meny.jsx
+++ src/pages/front/meny/Meny.jsx
@@ -1,8 +1,12 @@
-import React from "react";
 import logo from "../../../pics/LOGO_PRINCIPAL_KOLVAJEN.png";
 import "./meny.css";
+import { useState, React } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 const Meny = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="meny">
       <img src={logo} alt="big logo" />
@@ -19,6 +23,32 @@ const Meny = () => {
         <div className="meny-navbar-kontakt">
           <a href="/kontakt"> Kontakt</a>
         </div>
+      </div>
+      <div className="meny-navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+
+        {toggleMenu && (
+          <div className="meny-navbar-smallscreen-overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="meny-navbar-smallscreen-links">
+              <a href="/"> Hem</a>
+
+              <a href="/om-oss"> Om Oss</a>
+
+              <a href="/tjanster"> Våra Tjänster</a>
+
+              <a href="/kontakt"> Kontakt</a>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/kontakt/KontaktInnehåll/KontaktInnehåll.jsx
+++ src/pages/kontakt/KontaktInnehåll/KontaktInnehåll.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./KontaktInnehåll.css";
+import kontaktaOss from "../../../pics/Kontakta oss.png";
 
 const KontaktInnehåll = () => {
-  return <div className="KontaktInnehåll">KontaktInnehåll</div>;
+  return (
+    <div className="KontaktInnehåll">
+      <div className="KontaktInnehåll-bild">
+        <img src={kontaktaOss} alt="kontakta oss" />
+      </div>
+      <div className="KontaktInnehåll-text">
+        <h2>Kontakt</h2>
+        <span>
+          Bästa sättet att kontakta oss är att komma in och träffa oss! <p />{" "}
+          Annars hittar du kontakt information längst ner på hemsidan.
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default KontaktInnehåll;
